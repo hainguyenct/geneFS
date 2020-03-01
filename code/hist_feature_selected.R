59c3adc05699bcac3a5febf02848ea131ee41d00
Tinh Dao <dcongtinh@gmail.com>
2020-03-01 23:31:23 +0700

    Update font size label histogram

--- code/hist_feature_selected.R
+++ code/hist_feature_selected.R
@@ -2,15 +2,15 @@
 path_machine = "/Users/dcongtinh/gene-abundance/experiment/results/fc_model_with_feature_selected/qtf_pc576_10_fillseqf_nb10_auy_gray/"
 path_r =path_machine
 setwd(path_r)
-dataset_name <- "T2dgene"
+dataset_name <- "Wt2dgene"
 file_name <- paste0('feature_selected_', tolower(dataset_name), '.csv')
 table = read.csv(file_name, header = TRUE)
 dim(table)
 #step 2: visualize feature selected####
 library(ggplot2)
-title_size <- 30
-font_size <- 20
-ggplot(data=table, aes(x=freq)) + ylab("Count")+ xlab("Frequency of selected times") + 
+title_size <- 32
+font_size <- 24
+ggplot(data=table, aes(x=freq)) + ylab("The number of features")+ xlab("Frequency of selected times") + 
    geom_bar() + stat_count(aes(y=..count..,label=..count..),geom="text",vjust=-0.8,size=5.5) +
       theme(axis.text=element_text(size=font_size),
           axis.title=element_text(size=font_size,face="bold"),
